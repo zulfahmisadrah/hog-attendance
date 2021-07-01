0a01aef8e5ccca082be034243620ec999307858e
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-07-01 23:59:42 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.less';
+import {useSelector} from "react-redux";
+import {AdminContainer, MainContainer} from "./Containers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const userRole = useSelector(state => state.auth.user.role);
+
+    return userRole < 3 ? <AdminContainer/> : <MainContainer/>
 }
 
 export default App;
