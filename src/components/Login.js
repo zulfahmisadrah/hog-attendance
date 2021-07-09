41ea3d10bfef9d016ecb53538a5d2d5f917c0703
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-07-09 23:14:53 +0800

    create Login and Register page

---
+++ src/components/Login.js
@@ -0,0 +1,191 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {adminRootPath, registerPath, rootPath} from "../path";
+import {Alert, Button, Card, Col, Divider, Form, Input, Row, Typography, Space} from "antd";
+import styled from 'styled-components';
+import LockOutlined from "@ant-design/icons/lib/icons/LockOutlined";
+import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
+import {showErrorModal} from "../utils/Commons";
+import {getToken, removeAuth, storeAuth} from "../services/auth"
+import { authLogin, fetchUserData, setAuthToken } from '../services/client';
+import { useDispatch } from 'react-redux';
+
+const { Text} = Typography;
+
+const styles = {
+    backgroundColor: '#f8f8f8'
+}
+
+const StyledRow = styled(Row)`
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    .card-header{
+        margin-bottom: 16px;
+    }
+    .logo{
+        width: 70%;
+        align-self: center;
+    },
+    .input{
+        padding: .7em;
+        border-radius: 4px;
+    },
+    .ant-form-item{
+        margin-bottom: 16px;
+    },
+    .ant-divider{
+        margin: 0 0;
+    }
+    .login-form-button {
+        width: 100%;
+        height: auto;
+        padding: .7em;
+        border-radius: 5px;
+    },
+    .alert{
+        margin-bottom: 5px;
+    }
+`
+
+const StyledCard = styled(Card)`
+    min-width: 320px;
+    max-width: 340px;
+    border-radius: .25rem;
+    box-shadow: 2px 6px 15px 0 rgba(69,65,78,.1);
+    .card-header{
+        display: flex;
+        justify-content: center;
+    }
+`
+
+function Login() {
+    const dispatch = useDispatch();
+    let history = useHistory();
+
+    let [messages, setMessages] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const loginUser = (values) => {
+        authLogin(values).then( res => {
+            const userAuthData = res.data
+            storeAuth(userAuthData)
+            const token = getToken()
+            setAuthToken(token)
+            fetchUserData().then(userData => {
+                const user = userData.data
+                if (user.emailVerified) {
+                    const userRole = user.role.id
+                    const payload = {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role.id
+                    }
+                    dispatch({type: "SET_LOGIN", payload: payload})
+                    setIsLoading(false)
+                    const destination = userRole < 3 ? adminRootPath : rootPath
+                    history.push(destination)
+                } else {
+                    removeAuth()
+                    setMessages({emailNotVerified: (<div>Anda belum melakukan verifikasi email. Silakan cek email Anda.<br/><br/>Atau<Button type="link" size="small">Klik disini</Button>untuk mengirim ulang email verifikasi.</div>)})
+                    setIsLoading(false)
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                showErrorModal(e)
+                setIsLoading(false)
+            })
+        }).catch(e => {
+            showErrorModal(e.message)
+            console.log(e.response)
+            setIsLoading(false)
+        })
+    }
+
+    const resendEmailVerification = (user) => {
+    }
+
+    const onFinish = (values) => {
+        setIsLoading(true);
+        loginUser(values)
+    }
+
+    return (
+        <div style={styles}>
+            <StyledRow>
+                <Col>
+                    <StyledCard>
+                        <div className="card-header">
+                            <h2>ATTENDANCE SYSTEM</h2>
+                        </div>
+                        <Divider/>
+                        <div>
+                            <Form
+                                name="normal_login"
+                                className="login-form"
+                                initialValues={{
+                                    remember: true,
+                                }}
+                                onFinish={onFinish}
+                            >
+                                {messages.error ? (
+                                    <Alert className="alert" message={messages.error} type="error"/>
+                                ) : null}
+                                {messages.emailNotVerified ? (
+                                    <Alert className="alert" message={messages.emailNotVerified} type="error"/>
+                                ) : null}
+                                {messages.success ? (
+                                    <Alert className="alert" message={messages.success} type="success"/>
+                                ) : null}
+                                <Form.Item
+                                    name="email"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Masukkan email Anda',
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        prefix={<UserOutlined className="site-form-item-icon"/>}
+                                        className="input"
+                                        placeholder="Email"
+                                    />
+                                </Form.Item>
+                                <Form.Item
+                                    name="password"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Masukkan password Anda',
+                                        },
+                                    ]}
+                                >
+                                    <Input.Password
+                                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                                        className="input"
+                                        placeholder="Password"
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" className="login-form-button"
+                                            loading={isLoading}>
+                                        Masuk
+                                    </Button>
+                                </Form.Item>
+                                <Space align="center">
+                                    <Text>Belum memiliki akun? </Text>
+                                    <Link to={registerPath}>Daftar di sini!</Link>
+                                </Space>
+                            </Form>
+                        </div>
+                    </StyledCard>
+                </Col>
+            </StyledRow>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,266 @@
+import React, {useState} from 'react';
+import {Link as LinkRouter} from 'react-router-dom'
+import {DB_USERS, userCategory, userProgram} from "../utils/Constants";
+import {loginPath} from "../path";
+import {Alert, Button, Card, Col, Divider, Form, Input, Row, Typography, Space, Select} from "antd";
+import styled from 'styled-components';
+import {getImage} from "../assets/images";
+import {getCurrentDateTime} from "../utils/Commons";
+import {User, userConverter} from "../models";
+
+const { Text } = Typography;
+
+const styles = {
+    backgroundColor: '#f8f8f8'
+}
+
+const StyledRow = styled(Row)`
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    .logo{
+        width: 20%;
+        align-self: center;
+    },
+    .input{
+        padding: .5em;
+        border-radius: 4px;
+    },
+    .ant-form-item{
+        margin-bottom: 16px;
+    },
+    .ant-divider{
+        margin: 10px 0;
+    }
+    .login-form-button {
+        width: 100%;
+        height: auto;
+        padding: .5em;
+        border-radius: 5px;
+    },
+    .alert{
+        margin-bottom: 5px;
+    }
+`
+
+const StyledCard = styled(Card)`
+    width: 100%;
+    border-radius: .25rem;
+    box-shadow: 2px 6px 15px 0 rgba(69,65,78,.1);
+    .card-header{
+        display: flex;
+        justify-content: center;
+    }
+`
+
+function Register() {
+    let [messages, setMessages] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const registerUser = (values) => {
+        // auth.createUserWithEmailAndPassword(values.email, values.password)
+        //     .then((credential) => {
+        //         const user = credential.user
+        //         user.sendEmailVerification().then(() => {
+        //             values.role = 4
+        //             setMessages({success: "Pendaftaran berhasil. Silakan cek email Anda untuk melakukan verifikasi akun."})
+        //             const currentTime = getCurrentDateTime();
+        //             const newUser = new User(values)
+        //             newUser.createdAt = currentTime
+        //             newUser.updatedAt = currentTime
+        //             db.collection(DB_USERS).doc(user.uid).withConverter(userConverter).set(newUser).then(() => {
+        //                 auth.signOut()
+        //                 setIsLoading(false)
+        //             }).catch((error) => {
+        //                 console.error("Error adding document: ", error);
+        //                 setIsLoading(false)
+        //             })
+        //         })
+        //     }).catch((error) => {
+        //         console.log("Error creating user: ", error)
+        //         switch (error.code){
+        //             case "auth/email-already-in-use": setMessages({error: "Email telah terdaftar"}); break;
+        //             default: setMessages({error: error.message});
+        //         }
+        //         setIsLoading(false)
+        //     });
+    }
+
+    const onFinish = (values) => {
+        setIsLoading(true);
+        registerUser(values)
+    }
+
+    const generateUserProgramSelector = () => (
+        <Select placeholder="Program">
+            {userProgram.map(value => (
+                <Select.Option key={value} value={value}>{value}</Select.Option>
+            ))}
+        </Select>
+    )
+
+    const generateUserCategorySelector = () => (
+        <Select placeholder="Kelompok Ujian">
+            {userCategory.map(value => (
+                <Select.Option key={value} value={value}>{value}</Select.Option>
+            ))}
+        </Select>
+    )
+
+    return (
+        <div style={styles}>
+            <StyledRow>
+                <Col xs={20} lg={12}>
+                    <StyledCard>
+                        <div className="card-header">
+                            <img className="logo" src={getImage('logoDark')} alt="logo"/>
+                        </div>
+                        <Divider/>
+                        <div>
+                            <Form
+                                name="normal_login"
+                                className="login-form"
+                                layout={"vertical"}
+                                initialValues={{
+                                    remember: true,
+                                }}
+                                onFinish={onFinish}
+                            >
+                                {messages.error ? (
+                                    <Alert className="alert" message={messages.error} type="error"/>
+                                ) : null}
+                                {messages.success ? (
+                                    <Alert className="alert" message={messages.success} type="success"/>
+                                ) : null}
+                                <Row gutter={[20,0]}>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="name"
+                                            label="Nama"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Masukkan nama Anda',
+                                                    whitespace: true,
+                                                },
+                                            ]}
+                                        >
+                                            <Input className="input" placeholder="Name"/>
+                                        </Form.Item>
+                                    </Col>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="email"
+                                            label="Email"
+                                            rules={[
+                                                {
+                                                    type: 'email',
+                                                    message: 'Email tidak valid',
+                                                },
+                                                {
+                                                    required: true,
+                                                    message: 'Masukkan email Anda',
+                                                },
+                                            ]}
+                                        >
+                                            <Input className="input" placeholder="Email"/>
+                                        </Form.Item>
+                                    </Col>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="program"
+                                            label="Program"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Pilih program yang Anda ikuti',
+                                                },
+                                            ]}
+                                        >
+                                            {generateUserProgramSelector()}
+                                        </Form.Item>
+                                    </Col>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="category"
+                                            label="Kelompok Ujian"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Pilih kelompok ujian Anda',
+                                                },
+                                            ]}
+                                        >
+                                            {generateUserCategorySelector()}
+                                        </Form.Item>
+                                    </Col>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="password"
+                                            label="Password"
+                                            rules={[
+                                                {
+                                                    min: 6,
+                                                    message: 'Password minimal 6 karakter',
+                                                },
+                                                {
+                                                    required: true,
+                                                    message: 'Masukkan password Anda',
+                                                },
+                                            ]}
+                                        >
+                                            <Input.Password className="input" placeholder="Password"/>
+                                        </Form.Item>
+                                    </Col>
+                                    <Col xs={{span: 24}} lg={{span: 12}}>
+                                        <Form.Item
+                                            name="confirmPassword"
+                                            label="Konfirmasi Password"
+                                            dependencies={['password']}
+                                            hasFeedback
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Konfirmasi password Anda',
+                                                },
+                                                ({getFieldValue}) => ({
+                                                    validator(_, value) {
+                                                        if (!value || getFieldValue('password') === value) {
+                                                            return Promise.resolve();
+                                                        }
+                                                        return Promise.reject('Password tidak sesuai')
+                                                    }
+                                                })
+                                            ]}
+                                        >
+                                            <Input.Password className="input" placeholder="Confirm Password"/>
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                                <Divider/>
+                                <Row justify="center">
+                                    <Col xs={{span: 24, order: 2}} lg={{span: 16, order: 1}}>
+                                        <Space align="center">
+                                            <Text>Sudah memiliki akun? </Text>
+                                            <LinkRouter to={loginPath}>Masuk di sini!</LinkRouter>
+                                        </Space>
+                                    </Col>
+                                    <Col xs={{span: 24, order: 1}} lg={{span: 6, order: 2}}>
+                                        <Form.Item>
+                                            <Button type="primary" htmlType="submit" className="login-form-button"
+                                                    loading={isLoading}>
+                                                Daftar
+                                            </Button>
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
+                    </StyledCard>
+                </Col>
+            </StyledRow>
+        </div>
+    );
+}
+
+export default Register;
