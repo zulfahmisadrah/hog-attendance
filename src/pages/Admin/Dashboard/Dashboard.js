52f0e196bef07fbc531a501c481eaa519ccd3f94
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-12-13 23:18:58 +0800

    Admin/Dashboard : update interface

--- src/pages/Admin/Dashboard/Dashboard.js
+++ src/pages/Admin/Dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {Card, Typography} from "antd";
 
 export function Dashboard(){
-
+    const username = useSelector(state => state.auth.user.name);
 
     return (
-        <h1>Dashboard</h1>
+        <Card>
+            <h1>Dashboard</h1>
+            <Typography.Text strong>Halo, {username}</Typography.Text>
+        </Card>
     );
 
 }
