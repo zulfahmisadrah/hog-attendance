7af5da6a613788a943abbe20c16ac42a8780d8fb
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-12-14 22:27:35 +0800

    ButtonUploadDatasets : remove props data and add onSubmit

--- src/pages/Admin/Datasets/components/ButtonUploadDatasets.js
+++ src/pages/Admin/Datasets/components/ButtonUploadDatasets.js
@@ -4,30 +4,26 @@ import PropTypes from "prop-types";
 import {UploadImagesModal} from "./UploadImagesModal";
 
 ButtonUploadDatasets.propTypes = {
-    data: PropTypes.object.isRequired
+    onSubmit: PropTypes.func.isRequired
 }
 
 export function ButtonUploadDatasets(props) {
-    const {data} = props
+    const {onSubmit} = props
 
     const [visible, setVisible] = useState(false);
-
-    const showUploadModal = () => {
-        showModal();
-    }
 
     const showModal = () => setVisible(true)
     const closeModal = () => setVisible(false)
 
     return (
         <>
-            <Button className="w-100" size="large" onClick={showUploadModal}>
+            <Button className="w-100" size="large" onClick={showModal}>
                 Upload Foto
             </Button>
             {visible && (
                 <UploadImagesModal
-                    data={data}
                     visible={visible}
+                    onSubmit={onSubmit}
                     onCancel={closeModal}
                 />
             )}
