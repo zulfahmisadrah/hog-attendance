463f9ba89218ebee09647cae0c0158bfd84cfdff
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-12-14 21:18:23 +0800

    UploadImagesModal : remove data parameter and handle submit on parent component

--- src/pages/Admin/Datasets/components/UploadImagesModal.js
+++ src/pages/Admin/Datasets/components/UploadImagesModal.js
@@ -1,22 +1,20 @@
 import React, {useState} from "react";
 import {Form, Upload} from "antd";
 import PropTypes from "prop-types";
-import {DatasetService} from "../../../../services/services";
-import {showDataAddedNotification} from "../../../../utils/Commons";
 import {FormModal} from "../../../../components";
 import {InboxOutlined} from "@ant-design/icons";
 
 UploadImagesModal.propTypes = {
     visible: PropTypes.bool,
-    onCancel: PropTypes.func.isRequired,
+    maxSize: PropTypes.number,
+    onSubmit: PropTypes.func,
+    onCancel: PropTypes.func,
 }
 
 export function UploadImagesModal(props) {
-    const {data, visible, onCancel} = props
+    const {visible, maxSize, onSubmit, onCancel} = props
 
     const [fileList, setFileList] = useState([]);
-
-    const datasetService = new DatasetService();
 
     const handleCancel = () => {
         setFileList([]);
@@ -31,26 +29,26 @@ export function UploadImagesModal(props) {
         const index = fileList.indexOf(file);
         const newFileList = fileList.slice();
         newFileList.splice(index, 1);
-        setFileList(prevState => ([...prevState, newFileList]))
+        setFileList(prevState => ([...prevState, newFileList]));
     }
 
     const imageValidator = (_, value) => {
         if (value) {
-            const file = value[0]
+            const file = value[0];
             if (file) {
                 let message = "";
 
                 const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
                 if (!isJpgOrPng) {
-                    message = 'You can only upload JPG/PNG file!'
+                    message = 'You can only upload JPG/PNG file!';
                 }
 
-                const isLt2M = file.size / 1024 / 1024 < 2;
-                if (!isLt2M) {
-                    message = 'Image size must be smaller than 2MB!'
+                const isSizeValid = file.size / 1024 / 1024 < maxSize;
+                if (!isSizeValid) {
+                    message = `Image size must be smaller than ${maxSize} MB!`;
                 }
 
-                return isJpgOrPng && isLt2M ? Promise.resolve() : Promise.reject(message)
+                return isJpgOrPng && isSizeValid ? Promise.resolve() : Promise.reject(message);
             }
         }
         return Promise.resolve()
@@ -60,31 +58,13 @@ export function UploadImagesModal(props) {
         return false;
     }
 
-    const handleSubmit = (values, onSuccess) => {
-        if (values.fileList) {
-            values.fileList.forEach((file, index) => {
-                const fileData = file.originFileObj;
-                console.log(fileData)
-                const formData = new FormData();
-                formData.append('username', data);
-                formData.append('file', fileData);
-                datasetService.datasetCapture(formData, (file_path) => {
-                    console.log(`response = `, file_path);
-                    if (index === values.fileList.length-1) {
-                        showDataAddedNotification();
-                        onSuccess();
-                    }
-                })
-            })
-        }
-    }
-
     return (
         <FormModal
             title="Upload Foto"
             visible={visible}
+            okText="Submit"
             onCancel={handleCancel}
-            onSubmit={handleSubmit}
+            onSubmit={onSubmit}
             onFinish={handleCancel}
             width={500}
         >
@@ -92,7 +72,7 @@ export function UploadImagesModal(props) {
                 name="fileList"
                 valuePropName="fileList"
                 getValueFromEvent={normFile}
-                extra="Max. 2 MB"
+                extra={`Max. ${maxSize} MB`}
                 rules={[{validator: imageValidator}]}
             >
                 <Upload.Dragger
