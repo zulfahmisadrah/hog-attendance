33954595a42842c04414fe43b0c06110f84a7149
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-01-07 23:21:47 +0800

    set webcam size based on windows size

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,19 +1,27 @@
 import React, {forwardRef} from 'react';
 import Webcam from "react-webcam";
+import useWindowSize from "../utils/useWindowSize";
 
 function WebcamCapture(props, ref) {
+    const size = useWindowSize();
+    const isLandscape = size.height <= size.width;
+    const ratio = isLandscape ? size.width / size.height : size.height / size.width;
 
     const videoConstraints = {
-        // height: 1800,
-        // width: 3200,
-        facingMode: {exact: "environment"}
+        facingMode: {exact: "environment"},
+        aspectRatio: ratio
     }
 
     return (
         <Webcam
             audio={false}
             ref={ref}
+            // height={size.height}
+            width={size.width}
             screenshotFormat = "image/jpeg"
+            forceScreenshotSourceSize="true"
+            minScreenshotHeight={1200}
+            minScreenshotWidth={1200}
             videoConstraints = {videoConstraints}
             {...props}
         />
