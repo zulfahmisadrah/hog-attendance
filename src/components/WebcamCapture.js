c40a7f311dc98cefaa24845fb796fe829cb18350
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-01-17 22:16:46 +0800

    WebcamCapture : add facingmode and fix size issue

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -3,12 +3,14 @@ import Webcam from "react-webcam";
 import useWindowSize from "../utils/useWindowSize";
 
 function WebcamCapture(props, ref) {
+    const {facingMode, ...rest} = props;
+    
     const size = useWindowSize();
     const isLandscape = size.height <= size.width;
     const ratio = isLandscape ? size.width / size.height : size.height / size.width;
 
     const videoConstraints = {
-        facingMode: {exact: "environment"},
+        facingMode: {exact: facingMode},
         aspectRatio: ratio
     }
 
@@ -16,14 +18,13 @@ function WebcamCapture(props, ref) {
         <Webcam
             audio={false}
             ref={ref}
-            // height={size.height}
-            width={size.width}
+            style={{maxWidth: size.width}}
             screenshotFormat = "image/jpeg"
             forceScreenshotSourceSize="true"
             minScreenshotHeight={1200}
             minScreenshotWidth={1200}
             videoConstraints = {videoConstraints}
-            {...props}
+            {...rest}
         />
     )
 }
