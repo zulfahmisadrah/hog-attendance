27e9f2d1ac02b11e96bff69e01573f4717f5e956
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-01-24 23:06:27 +0800

    ColumnActions : add feature to modify action visibility

--- src/components/ColumnActions.js
+++ src/components/ColumnActions.js
@@ -4,19 +4,27 @@ import {ColumnActionDetails} from "./ColumnActionDetails";
 import {ColumnActionEdit} from "./ColumnActionEdit";
 import {ColumnActionDelete} from "./ColumnActionDelete";
 
+const initialVisible = {
+    details: true,
+    edit: true,
+    delete: true
+}
 
-export const ColumnActions = ({detailRows, formModal, onUpdate, onConfirmDelete}) => ({
-    key: 'action',
-    title: 'Action',
-    isDummyField: true,
-    width: 80,
-    fixed: 'right',
-    align: 'center',
-    render: (_, record) => (
-        <Space wrap>
-            <ColumnActionDetails data={record} detailRows={detailRows} />
-            <ColumnActionEdit formModal={formModal} data={record} onSubmit={onUpdate} />
-            <ColumnActionDelete data={record} onConfirm={onConfirmDelete} />
-        </Space>
-    )
-})
+export const ColumnActions = ({detailRows, formModal, onUpdate, onConfirmDelete, visible = initialVisible}) => {
+    const isVisible = {...initialVisible, ...visible}
+    return ({
+        key: 'action',
+        title: 'Action',
+        isDummyField: true,
+        width: 80,
+        fixed: 'right',
+        align: 'center',
+        render: (_, record) => (
+            <Space wrap>
+                {isVisible.details && <ColumnActionDetails data={record} detailRows={detailRows}/>}
+                {isVisible.edit && <ColumnActionEdit formModal={formModal} data={record} onSubmit={onUpdate}/>}
+                {isVisible.delete && <ColumnActionDelete data={record} onConfirm={onConfirmDelete}/>}
+            </Space>
+        )
+    })
+}
