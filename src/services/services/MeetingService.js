7c71ce05e5cb23f8ba8d3e0b7e0cac4ed05dbf04
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-06-03 23:32:23 +0800

    MeetingService : update get list meeting for all types

--- src/services/services/MeetingService.js
+++ src/services/services/MeetingService.js
@@ -10,19 +10,23 @@ export class MeetingService extends APIService {
         super(meetingRequest);
     }
 
-    getListTodayMeeting = ({offset, limit, onSuccess, onError}) => {
-        this.apiRequest.getMyMeetings(offset, limit).then(res => {
+    getNearestMeeting = ({offset, limit, onSuccess, onError}) => {
+        this.apiRequest.getMyMeetingsNearest(offset, limit).then(res => {
             const data = res.data
-            const listData = []
-            const currentDate = getMoment()
-            data.forEach((value) => {
-                const schedule = getDateTimeFromString(value.date, dateTimeFormat)
-                const isActive = currentDate.isSame(schedule, 'days')
-                // const isActive = currentDate.diff(schedule) > -43200000 // waktu sekarang - jadwal > -12 jam
-                const isNotFinished = value.status !== MeetingStatus.Selesai
-                if (isActive && isNotFinished) listData.push(value)
-            })
-            onSuccess(listData)
+            onSuccess(data)
+        }).catch(e => {
+            if (onError instanceof Function){
+                onError(e)
+            } else {
+                console.log("getListTodayMeeting", e)
+            }
+        })
+    }
+
+    getListTodayMeeting = ({offset, limit, onSuccess, onError}) => {
+        this.apiRequest.getMyMeetingsToday(offset, limit).then(res => {
+            const data = res.data
+            onSuccess(data)
         }).catch(e => {
             if (onError instanceof Function){
                 onError(e)
@@ -33,17 +37,9 @@ export class MeetingService extends APIService {
     }
 
     getListScheduledMeeting = ({offset, limit, onSuccess, onError}) => {
-        this.apiRequest.getMyMeetings(offset, limit).then(res => {
+        this.apiRequest.getMyUpcomingMeetings(offset, limit).then(res => {
             const data = res.data
-            const listData = []
-            const currentDate = getMoment()
-            data.forEach((value) => {
-                const schedule = getDateTimeFromString(value.date, dateTimeFormat)
-                const isScheduled = currentDate.isBefore(schedule, 'days')
-                // const isUpcoming = currentDate.diff(schedule) <= -43200000 // lebih dari 12 jam menuju jadwal
-                if (isScheduled) listData.push(value)
-            })
-            onSuccess(listData)
+            onSuccess(data)
         }).catch(e => {
             if (onError instanceof Function){
                 onError(e)
@@ -54,16 +50,9 @@ export class MeetingService extends APIService {
     }
 
     getListFinishedMeeting = ({offset, limit, onSuccess, onError}) => {
-        this.apiRequest.getMyMeetings(offset, limit).then(res => {
+        this.apiRequest.getMyFinishedMeetings(offset, limit).then(res => {
             const data = res.data
-            const listData = []
-            data.forEach((value) => {
-                const isFinished = value.status === MeetingStatus.Selesai
-                if (isFinished) {
-                    listData.push(value)
-                }
-            })
-            onSuccess(listData)
+            onSuccess(data)
         }).catch(e => {
             if (onError instanceof Function){
                 onError(e)
